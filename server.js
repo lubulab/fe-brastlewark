0594fba241f808bcce5526fc24ebe82469e9d2d3
Davide Vacatello <davide.vacatello@gmail.com>
2018-12-03 23:20:21 +0100

    heroku

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/bower_components'));
 
 //add this so the browser can GET the bower files
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
